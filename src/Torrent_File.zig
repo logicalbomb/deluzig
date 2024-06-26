34c4ca7c0c931e103e16a425c9e8f044633cf899
Zac Propersi <3389111+logicalbomb@users.noreply.github.com>
2024-06-26 01:43:17 -0700

    Adjusting the impl to be more friendly

--- src/Torrent_File.zig
+++ src/Torrent_File.zig
@@ -58,6 +58,23 @@ pub fn printSummary(self: Torrent_File, writer: anytype) !void {
     try writer.print("Summary complete.\n", .{});
 }
 
+
+const TFIndexContext = struct {
+    // TODO: can we trash the stack and go recursive?
+    stack_depth: u8 = 0xFF,
+    stack_type: []u8 = undefined,
+    stack_pos: []u64 = undefined,
+
+    is_key: bool = false,
+
+    kv_num: u8 = 0,
+    key_slices: [][]u8 = undefined,
+    key_pos: []u64 = undefined,
+    val_slices: [][]u8 = undefined,
+    val_ints: []u64 = undefined,
+};
+
+
 // Parse the torrent file and index the important parts
 // More info on the format can be found here: https://wiki.theory.org/BitTorrentSpecification
 // TODO: get smarter about dynamic allocations?!?
@@ -82,88 +99,76 @@ fn index(allocator: std.mem.Allocator, contents: []u8) !Torrent_File {
     // x created by - s
     // x encoding - s
 
+
     print("Starting the index.\n", .{});
 
-    var stack_depth: u8 = 0xFF;
-    var stack_type = [_]u8{0xFF} ** ((1 << 9) - 1);
-    var stack_pos = [_]u64{0xFFFFFFFFFFFFFFFF} ** ((1 << 9) - 1);
-
-    var is_key: bool = false;
-
-    var kv_num: u8 = 0;
-    var key_slices = [_][]u8{contents[0..10]} ** ((1 << 9) - 1);
-    var key_pos = [_]u64{0xFFFFFFFFFFFFFFFF} ** ((1 << 9) - 1);
-    var val_slices = [_][]u8{contents[0..10]} ** ((1 << 9) - 1);
-    var val_ints = [_]u64{0xFFFFFFFFFFFFFFFF} ** ((1 << 9) - 1);
-
-    var result: Torrent_File = .{ ._contents = contents };
+    var context = TFIndexContext{};
+    // TODO: I bet this cleans up better if we allocate one of these!
+    // Shenanigans about creating non-const slices
+    const shenanigan: u64 = @as(u64, contents.len);
+    context.stack_type = &[_]u8{contents[0]} ** ((1 << 9) - 1);
+    context.stack_pos = &[_]u64{shenanigan} ** ((1 << 9) - 1);
+    context.key_slices = &[_][]u8{contents[0..2]} ** ((1 << 9) - 1);
+    context.key_pos = &[_]u64{shenanigan} ** ((1 << 9) - 1);
+    context.val_slices = &[_][]u8{contents[0..2]} ** ((1 << 9) - 1);
+    context.val_ints = &[_]u64{shenanigan} ** ((1 << 9) - 1);
 
     var byte_pos: u64 = 0;
     while (byte_pos < contents.len) {
         const byte: u8 = contents[byte_pos];
         switch (byte) {
             // #'s for string len
-            0x30...0x39 => {
-                const decoded = try bDecodeInteger(contents, byte_pos, 0x3A);
-                const str_val = contents[(decoded[0] + 1)..(decoded[0] + 1 + decoded[1])];
-                assert(str_val.len == decoded[1]);
-                if (is_key) {
-                    key_slices[kv_num] = str_val;
-                    key_pos[kv_num] = byte_pos;
-                    kv_num += 1;
-                } else {
-                    val_slices[kv_num - 1] = str_val;
-                }
-                byte_pos = decoded[0] + decoded[1];
+            '0'...'9' => {
+                byte_pos = try parseString(&context, contents, byte_pos);
             },
             // d for dictionary - keys are always strings or l for list
-            0x64, 0x6C => {
-                stack_depth = @addWithOverflow(stack_depth, 1)[0];
-                stack_type[stack_depth] = byte;
-                stack_pos[stack_depth] = byte_pos;
+            'd', 'l' => {
+                context.stack_depth = @addWithOverflow(context.stack_depth, 1)[0];
+                context.stack_type[context.stack_depth] = byte;
+                context.stack_pos[context.stack_depth] = byte_pos;
                 // TODO: figure out how to properly handle these vals
             },
             // e for end of encoding for a dictionary or a list
-            0x65 => {
-                stack_type[stack_depth] = 0xFF;
-                stack_pos[stack_depth] = 0xFFFFFFFFFFFFFFFF;
-                const res = @subWithOverflow(stack_depth, 1);
-                stack_depth = res[0];
+            'e' => {
+                context.stack_type[context.stack_depth] = 0xFF;
+                context.stack_pos[context.stack_depth] = 0xFFFFFFFFFFFFFFFF;
+                const res = @subWithOverflow(context.stack_depth, 1);
+                context.stack_depth = res[0];
                 // If we popped back to a dictionary, we need to prepare for a key
-                if (stack_type[stack_depth] == 0x64) { is_key = false; }
+                if (context.stack_type[context.stack_depth] == 'd') { context.is_key = false; }
             },
             // i for integer
-            0x69 => {
-                byte_pos += 1;
-                const decoded = try bDecodeInteger(contents, byte_pos, 0x65);
-                val_ints[kv_num - 1] = decoded[1];
-                byte_pos = decoded[0];
+            'i' => {
+                byte_pos = try parseInteger(&context, contents, byte_pos);
             },
             else => unreachable,
         }
         byte_pos += 1;
         // Flip between key or val when we are on a dictionary
-        if (stack_type[stack_depth] == 0x64) { is_key = !is_key; }
+        if (context.stack_type[context.stack_depth] == 'd') { context.is_key = !context.is_key; }
     }
 
-    assert(stack_depth == 255);
-    assert(stack_type[0] == 0xFF);
-    assert(stack_pos[0] == 0xFFFFFFFFFFFFFFFF);
+    assert(context.stack_depth == 255);
+    assert(context.stack_type[0] == 0xFF);
+    assert(context.stack_pos[0] == 0xFFFFFFFFFFFFFFFF);
+
+    var result: Torrent_File = .{ ._contents = contents };
 
     // Hydrate the result's fields from the parse
     var found_fields: u8 = 0;
     const torrent_fields = comptime std.meta.fields(@TypeOf(result));
-    for (0..kv_num) |i| {
-        const key = key_slices[i];
+    for (0..context.kv_num) |i| {
+        const key = context.key_slices[i];
         std.mem.replaceScalar(u8, key, ' ', '_');
         inline for (torrent_fields) |field| {
             if (std.mem.eql(u8, key, field.name)) {
                 @field(result, field.name) = switch (field.type) {
-                    ?[]u8 => val_slices[i],
-                    ?u64 => val_ints[i],
+                    ?[]u8 => context.val_slices[i],
+                    ?u64 => context.val_ints[i],
                     else => undefined,
                 };
-                key_slices[i][0] = 0x5F;
+                // TODO: do something better than a magic value here
+                context.key_slices[i][0] = '_';
                 found_fields += 1;
             }
         }
@@ -171,15 +176,15 @@ fn index(allocator: std.mem.Allocator, contents: []u8) !Torrent_File {
 
     // Record any unhandled keys
     var skips: u8 = 0;
-    result._unk_key_pos = try allocator.alloc(u64, kv_num - found_fields);
-    result._unk_key_val = try allocator.alloc([]u8, kv_num - found_fields);
-    for (0..kv_num) |i| {
-        if (key_slices[i][0] == 0x5F) {
+    result._unk_key_pos = try allocator.alloc(u64, context.kv_num - found_fields);
+    result._unk_key_val = try allocator.alloc([]u8, context.kv_num - found_fields);
+    for (0..context.kv_num) |i| {
+        if (context.key_slices[i][0] == '_') {
             skips += 1;
             continue;
         }
-        result._unk_key_pos.?[i - skips] = key_pos[i];
-        result._unk_key_val.?[i - skips] = key_slices[i];
+        result._unk_key_pos.?[i - skips] = context.key_pos[i];
+        result._unk_key_val.?[i - skips] = context.key_slices[i];
     }
 
     
@@ -190,11 +195,37 @@ fn index(allocator: std.mem.Allocator, contents: []u8) !Torrent_File {
     return result;
 }
 
+// Consume the integer at the pointer and return a pointer to the next char
+fn parseInteger(context: *TFIndexContext, contents: []u8, og_pos: u64) !u64 {
+    const pos = og_pos + 1;
+    const decoded = try bDecodeInteger(contents, pos, 'e');
+    context.val_ints[context.kv_num - 1] = decoded[1];
+    return decoded[0];
+}
+
+// Consume the string at the pointer and return a pointer to the next char
+fn parseString(context: *TFIndexContext, contents: []u8, og_pos: u64) !u64 {
+    const decoded = try bDecodeInteger(contents, og_pos, ':');
+    const str_val = contents[(decoded[0] + 1)..(decoded[0] + 1 + decoded[1])];
+    assert(str_val.len == decoded[1]);
+    if (context.is_key) {
+        context.key_slices[context.kv_num] = str_val;
+        context.key_pos[context.kv_num] = og_pos;
+        context.kv_num += 1;
+    } else {
+        context.val_slices[context.kv_num - 1] = str_val;
+    }
+    return decoded[0] + decoded[1];
+}
+
+// Can decode an integer and returns a tuple of the next index in the stream
+// to parse and the value of the integer
 fn bDecodeInteger(contents: []u8, og_pos: u64, delimiter: u8) !*const[2]u64 {
     var pos = og_pos;
     var check = contents[pos];
     while (check != delimiter) : ({ pos += 1; check = contents[pos]; }) {
-        assert((check >= 0x30 and check <= 0x39) or check == delimiter);
+        //print("Decoding an integer: pos={d}, val={c} del={c}\n", .{ pos, check, delimiter });
+        assert((check >= '0' and check <= '9') or check == delimiter);
     }
     return &[_]u64{ pos, try std.fmt.parseInt(u64, contents[og_pos..pos], 10) };
 }
